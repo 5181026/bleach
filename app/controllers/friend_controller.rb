f92bc94d7b1ef1a2ff94a165c42ec980192d6b77
5181026 <5181026@st.hsc.ac.jp>
2020-02-21 21:31:02 +0900

    コントローラの追加とグループ一覧html

---
+++ app/controllers/friend_controller.rb
@@ -0,0 +1,13 @@
+class FriendController < ApplicationController
+    #フレンド登録のコントローラ
+    def friend_add
+    end
+
+    #フレンド一覧のコントローラ
+    def friend_view
+    end
+
+    #フレンド情報のコントローラ
+    def friend_info_view
+    end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,17 @@
+class UserController < ApplicationController
+    # ユーザ登録のコントローラ
+    def create_user
+    end
+    
+    # ログインのコントローラ
+    def login_user
+    end
+
+    # ユーザ編集のコントローラ
+    def edit_user
+    end
+
+    #プロフィール表示のコントローラ
+    def user_view 
+    end
+end
