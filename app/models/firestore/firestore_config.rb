02b220fd25d0f9ab7d8d0cde606c61fd9c60cd36
5181026 <5181026@st.hsc.ac.jp>
2020-03-02 00:13:10 +0900

    ripository作業中

--- app/models/firestore/firestore_config.rb
+++ app/models/firestore/firestore_config.rb
@@ -1,6 +1,6 @@
 # firestoreに認証するためのprojectidをjsonファイルから取得するクラス
 class FirestoreConfig
-    def get_firestore_id
+    def self.get_firestore_id
         File.open(FireConst::FIRESTORE_JSON_PATH) do |file|
             
             return JSON.load(file)[FireConst::FIRE_ID]
--- app/models/repository/user_repository.rb
+++ app/models/repository/user_repository.rb
@@ -6,7 +6,23 @@ class UserRepository
         @firestore = firestore
     end
 
+    # 全てのユーザを配列で返す
     def get_all_user
-        return @firestore.get
+        users = [] 
+        @firestore.get do |user|
+            users << user.data
+        end 
+        return users
+    end
+
+    # 条件に一致したユーザを返す
+    def get_find_user
+        users = ""
+        query = @firestore.where "userid" , "=" , "test001"
+        query.get do |user|
+            puts user.data
+            users = user.data
+        end
+        return users
     end
 end
