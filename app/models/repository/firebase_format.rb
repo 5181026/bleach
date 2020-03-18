47dce6ea02096e48c28ea378507544e929c2c19a
5181026 <5181026@st.hsc.ac.jp>
2020-03-18 11:46:59 +0900

    ユーザ登録関数作成

--- app/models/repository/firebase_format.rb
+++ app/models/repository/firebase_format.rb
@@ -2,8 +2,14 @@
 
 # 途中
 module FirebaseFormat
+    include CreateTimestamp
     def user_col
         @firestore.col(FireConst::FIRE_COL_USERS)
+    end
+
+    def add_user_col(doc_id , col_name)
+        # @firestore.doc("#{FireConst::FIRE_COL_USERS}/#{doc_id}/#{col_name}")
+        @firestore.col("#{FireConst::FIRE_COL_USERS}").doc("#{doc_id}").col("#{col_name}")
     end
 
     def group_col
