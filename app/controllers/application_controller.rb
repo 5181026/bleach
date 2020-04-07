e2adffe7d0be25617816d6898a66af2088d26d10
5181026 <5181026@st.hsc.ac.jp>
2020-04-07 16:14:48 +0900

    フィルターを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authorize_user , except: [:login , :add_user]  #フィルターでアクションの処理の前にauthorize_userを処理する
+
+  protected
+  #ログインしていないユーザをloginに戻す
+  def authorize_user
+    unless session[:user].present?
+        redirect_to action: "login"
+    end
+  end
 end
