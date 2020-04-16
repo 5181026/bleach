53f587ce61abdc0a5dc2f537f687ba60f5e32093
5181026 <5181026@st.hsc.ac.jp>
2020-04-16 18:03:27 +0900

    本番環境のための出力コメント

--- app/models/firestore/firestore_connection.rb
+++ app/models/firestore/firestore_connection.rb
@@ -8,6 +8,7 @@ class FirestoreConnection
             # id = FirestoreConfig::get_firestore_id
             # return Google::Cloud::Firestore.new project_id: id
             # firebaseに環境変数を使って接続する。 
+            puts "環境変数===#{ENV['FIREBASE']}"
             return Google::Cloud::Firestore.new project_id: ENV['FIREBASE']
         rescue => e
             #サーバ立ち上げ時にFireStoreと接続できなければサーバを止める
