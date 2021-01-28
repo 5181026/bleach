a2d305a2b629d8cb259607b075d40c3cd8479115
5181026 <5181026@st.hsc.ac.jp>
2021-01-28 14:40:47 +0900

    Pathの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,17 @@ class ApplicationController < ActionController::Base
 
   before_action :authorize_user , except: [:login , :add_user]  #フィルターでアクションの処理の前にauthorize_userを処理する
 
-  # class FireBase < ActionController::ActionControllerError
-  # end 
-
-  # rescue_from FireBase, with: :firebase_not_connection
-  #　firebaseのエラー
-  # def firebase_not_connection
-  #   puts "接続に失敗しました"
-  # end
 rescue_from ActiveRecord::RecordNotFound, with: :authentication_error_handler
   def authentication_error_handler
     flash[:alert] = Constants::W_002
-    redirect_to action: :login
+    redirect_to controller:  :user , action: :login
   end
 
   protected
   #ログインしていないユーザをloginに戻す
   def authorize_user
     unless session[:user].present?
-        redirect_to action: :login
+        redirect_to controller:  :user , action: :login
     end
   end
 end
--- app/models/firestore/firestore_connection.rb
+++ app/models/firestore/firestore_connection.rb
@@ -5,8 +5,11 @@ require "google/cloud/firestore"
 class FirestoreConnection
     def self.get_connection
         begin
-            id = FirestoreConfig::get_firestore_id
-            return Google::Cloud::Firestore.new project_id: id
+            # id = FirestoreConfig::get_firestore_id
+            # return Google::Cloud::Firestore.new project_id: id
+            # firebaseに環境変数を使って接続する。 
+            puts "環境変数===#{ENV['FIREBASE']}"
+            return Google::Cloud::Firestore.new project_id: ENV['FIREBASE']
         rescue => e
             #サーバ立ち上げ時にFireStoreと接続できなければサーバを止める
             puts "エラー：FireBaseに接続できませんでした。アプリを終了します。"
